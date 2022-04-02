556f7f1819b75a3813ef51b9244bb1d405a4a220
Christina Yin <yinchristina373@gmail.com>
2022-04-01 21:01:44 -0700

    created a delete clones function to clear the copies inside of the copy container

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const copyContainer = document.querySelector('.copy-container');
 
 originalContainer.forEach(
     divClickEvt => divClickEvt.addEventListener('click', (evt) =>
-    {const itemCopied = evt.target.parentElement.outerHTML
-    copyContainer.insertAdjacentHTML("beforeend",itemCopied)
+    {const itemCopied = evt.target.parentElement.outerHTML;
+    copyContainer.insertAdjacentHTML("beforeend",itemCopied);
     })
 )
+
+const clearButton = document.querySelector('button');
+function delClones() {
+    copyContainer.innerHTML = "";
+}
+
+clearButton.addEventListener('click', delClones)
